992c3243775085b840d6f037dd3e60098540c5c9
Michael Merene <michael.merene@pivotfurniture.com>
2022-07-02 18:10:28 -0500

    connected api and added the interfaces

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-
+import React, { useEffect, useState } from 'react';
+import { Repo } from './Repo';
 import './App.css';
 
+interface IProps {
+  repos: Repo[];
+}
+
 export function App() {
+  const [data, setData] = useState<IProps>();
+  useEffect(() => {
+    fetch('http://localhost:4000/repos')
+      .then((response) => response.json())
+      .then((datas) => setData(datas));
+  }, []);
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Michael</h1>
+      {data?.repos.map((x, i) => {
+        return (
+          <>
+            <div>
+              <h1 key={i}>{x.name}</h1>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 }
